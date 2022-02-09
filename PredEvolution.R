d547c15c391b36afabff74233116cae9033e52d5
izabelnolau <nolau@dme.ufrj.br>
2022-02-09 17:11:10 -0300

    App

--- PredEvolution.R
+++ PredEvolution.R
@@ -14,22 +14,22 @@ ui <- fluidPage(
   ## add favicon
   tags$head(tags$link(rel = "icon", href = "img/favicon.ico")),
   
-  ## Título
-  titlePanel("Evolução da previsão COVID-19 - DEST/UFMG"),
+  ## Titulo
+  titlePanel("Evolucao da previsao COVID-19 - DEST/UFMG"),
   # tagList(
   #   tags$head(
   #     tags$title(
-  #       "Evolução da previsão COVID-19 - DEST/UFMG"
+  #       "Evolucao da previsao COVID-19 - DEST/UFMG"
   #     )
   #   )),
   
   fluidRow(
     column(width = 10, offset = 1,
            wellPanel(
-             h4("Selecione o país/estado que deseja investigar.", style = "text-align: center;"),
+             h4("Selecione o pais/estado que deseja investigar.", style = "text-align: center;"),
              h4("Select a country/state to analyse.", style = "text-align: center;"),
              HTML("<center><a href = 'http://est.ufmg.br/covidlp/home/pt/' style = 'color: #00b1d8; text-decoration: underline;'>Sobre/About</a> | <a href = 'http://est.ufmg.br/covidlp/home/pt/metodologia' style = 'color: #00b1d8; text-decoration: underline;'>Metodologia/Methodology</a></center>"),
-             # h5(sprintf("Última atualização/last update: %s", Sys.Date()), style = "text-align: center;"),
+             # h5(sprintf("Ultima atualizacao/last update: %s", Sys.Date()), style = "text-align: center;"),
              # HTML("<center><a href = 'http://est.ufmg.br/covidlp/home/pt/' style = 'color: #00b1d8; text-decoration: underline;'>CovidLP website</a> | <a href = 'https://github.com/CovidLP/app_COVID19' style = 'color: #00b1d8; text-decoration: underline;'>Source code</a> <br> <a href = 'http://est.ufmg.br/covidlp/home/pt/' style = 'color: #00b1d8; text-decoration: underline;'>Sobre/About</a> | <a href = 'http://est.ufmg.br/covidlp/home/pt/metodologia' style = 'color: #00b1d8; text-decoration: underline;'>Metodologia/Methodology</a></center>"),
              # HTML("<center><a href = 'http://est.ufmg.br/covidlp/home/pt/' style = 'color: #00b1d8; text-decoration: underline;'>CovidLP website</a></center>"),
              style = "background-color: #ffffff; border-radius: 4px; box-shadow: 0 0 4px 0 rgba(69,69,69,.2);"
@@ -68,7 +68,8 @@ ui <- fluidPage(
            align="center",
            div(
              class = "btn_div",
-             tipify(bsButton("pB2", "?", style = "inverse", size = "extra-small"), HTML("Selecione o país e o estado de interesse! <br> Select a country and a state!"))
+             tipify(bsButton("pB2", "?", style = "inverse", size = "extra-small"),
+                    HTML("Selecione o pais e o estado de interesse! <br> Select a country and a state!"))
            )),
     column(width=5,
            align='left',
@@ -90,15 +91,15 @@ ui <- fluidPage(
   
   # Show a plot of the generated distribution
   fluidRow(align = "center",
-           uiOutput("distPlot")),
-  fluidRow(
+           imageOutput("distPlot")),
+  fluidRow( 
     align = "center",
     sliderTextInput(
       "bins",
       "Data:",
-      choices = read.table(paste0("https://github.com/CovidLP/LTPred_Evolution/raw/main/GraphsPng/Brazil_n/date.vector.txt"))$V1,
+      choices = read.table(paste0("GraphsPng/Brazil_n/date.vector.txt"))$V1,
       animate = animationOptions(
-        interval = 1000,
+        interval = 750,
         loop = FALSE,
         playButton = NULL,
         pauseButton = NULL
@@ -131,20 +132,20 @@ server <- function(input, output, session) {
     
     if (input$pais!="Brazil") {
       if (input$tipo_data == TRUE) {
-        choices = read.table(paste0("https://github.com/CovidLP/LTPred_Evolution/raw/main/GraphsPng/", str_replace_all(input$pais," ","-"), "_n/date.vector.txt"))$V1
+        choices = read.table(paste0("GraphsPng/", str_replace_all(input$pais," ","-"), "_n/date.vector.txt"))$V1
       } else{
-        choices = read.table(paste0("https://github.com/CovidLP/LTPred_Evolution/raw/main/GraphsPng/", str_replace_all(input$pais," ","-"), "_d/date.vector.txt"))$V1
+        choices = read.table(paste0("GraphsPng/", str_replace_all(input$pais," ","-"), "_d/date.vector.txt"))$V1
       }
     }
     
     else if (input$state=="<all>") {
       if (input$tipo_data == TRUE) {
         
-        choices = read.table(paste0("https://github.com/CovidLP/LTPred_Evolution/raw/main/GraphsPng/", str_replace_all(input$pais," ","-"), "_n/date.vector.txt"))$V1
+        choices = read.table(paste0("GraphsPng/", str_replace_all(input$pais," ","-"), "_n/date.vector.txt"))$V1
         
       } else{
         
-        choices = read.table(paste0("https://github.com/CovidLP/LTPred_Evolution/raw/main/GraphsPng/", str_replace_all(input$pais," ","-"), "_d/date.vector.txt"))$V1
+        choices = read.table(paste0("GraphsPng/", str_replace_all(input$pais," ","-"), "_d/date.vector.txt"))$V1
         
       }
       
@@ -152,11 +153,11 @@ server <- function(input, output, session) {
       
       if (input$tipo_data == TRUE) {
         
-        choices = read.table(paste0("https://github.com/CovidLP/LTPred_Evolution/raw/main/GraphsPng/", input$pais,"_",input$state, "_ne/date.vector.txt"))$V1
+        choices = read.table(paste0("GraphsPng/", input$pais,"_",input$state, "_ne/date.vector.txt"))$V1
         
       } else{
         
-        choices = read.table(paste0("https://github.com/CovidLP/LTPred_Evolution/raw/main/GraphsPng/", input$pais,"_",input$state, "_de/date.vector.txt"))$V1
+        choices = read.table(paste0("GraphsPng/", input$pais,"_",input$state, "_de/date.vector.txt"))$V1
         
       }
     }
@@ -167,45 +168,35 @@ server <- function(input, output, session) {
     )
   }, ignoreInit = TRUE)
   
-  
-  # arquivo = reactive({
-  #   pais=as.character(input$pais)
-  #   pais=str_replace_all(pais," ","-")
-  #   if (pais!="Brazil") {
-  #     if (input$tipo_data == TRUE) {
-  #       endfile = "_n_graph.rds"
-  #     } else{
-  #       endfile = "_d_graph.rds"
-  #     }
-  #     arquivo = paste0(pais, endfile)
-  #   }
-  #   else if (input$state=="<all>") {
-  #     if (input$tipo_data == TRUE) {
-  #       endfile = "_n_graph.rds"
-  #     } else{
-  #       endfile = "_d_graph.rds"
-  #     }
-  #     arquivo = paste0(pais, endfile)
-  #   } else{
-  #     startfile = paste0(pais,"_")
-  #     state=as.character(input$state)
-  #     if (input$tipo_data == TRUE) {
-  #       endfile = "_ne_graph.rds"
-  #     } else{
-  #       endfile = "_de_graph.rds"
-  #     }
-  #     
-  #     arquivo = paste0(startfile, state, endfile)
-  #   }
-  #   return(arquivo)
-  # })
-  
-  output$distPlot <- renderUI({
+  outfile <- reactive({
     
-    tags$img(src = paste0("https://github.com/CovidLP/LTPred_Evolution/raw/main/GraphsPng/", str_replace_all(input$pais," ","-"), "_", ifelse(input$tipo_data, "n", "d"), "/", input$bins, ".png"))
-    # png::readPNG(url(paste0("https://github.com/CovidLP/LTPred_Evolution/raw/main/GraphsPng/", str_replace_all(input$pais," ","-"), "_n/", input$bins, ".png")))
+    url_init = "https://raw.githubusercontent.com/CovidLP/LTPred_Evolution/main/GraphsPng/"
+    
+    state = as.character(input$state)
+    
+    if (state != "<all>") {
+      local = paste0("Brazil_",
+                     as.character(input$state),
+                     "_",
+                     ifelse(input$tipo_data, "ne", "de"))
+    } else{
+      local = paste0(as.character(input$pais),
+                     "_",
+                     ifelse(input$tipo_data, "n", "d"))
+    }
+    
+    out = paste0(url_init, local, "/", as.character(input$bins), ".png")
+    return(out)
     
   })
+  
+  
+  output$distPlot <- renderImage({
+    img_file <- tempfile(fileext = ".png")
+    download.file(url = outfile(), destfile = img_file, mode = "wb", quiet = TRUE)
+    list(src = img_file)
+  }, deleteFile = TRUE)
+  
 }
 
 # Run the application
